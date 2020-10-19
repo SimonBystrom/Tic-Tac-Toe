dc2ab4af74482ff268b334993bd023f0a9fbab49
simonbystrom <simonbystrom@Simons-MacBook-Air.local>
2020-10-19 13:40:07 +0900

    Updates

---
+++ precache-manifest.8ca5b2c6da964d0c7970a53a2862c4c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b5ca5f2ae1112e3602b18e546d5b31",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "5f5533cc31fd70ca8c81",
+    "url": "/Tic-Tac-Toe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "67c60802556de53041d6",
+    "url": "/Tic-Tac-Toe/static/js/2.8fee2fd0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tic-Tac-Toe/static/js/2.8fee2fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5533cc31fd70ca8c81",
+    "url": "/Tic-Tac-Toe/static/js/main.a5d81fbe.chunk.js"
+  },
+  {
+    "revision": "fc0729295df9e52cea5c",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.836ad489.js"
+  }
+]);
